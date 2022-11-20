8ba204f6fa6945685ebd6ada0744792408aae0d2
Yonattan <34703013+lie16@users.noreply.github.com>
2022-11-20 14:34:35 +0700

    edit profile done

--- lib/module/tutorial/latihan/ltfm_edit_profile/view/ltfm_edit_profile_view.dart
+++ lib/module/tutorial/latihan/ltfm_edit_profile/view/ltfm_edit_profile_view.dart
@@ -10,7 +10,14 @@ class LtfmEditProfileView extends StatefulWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("LtfmEditProfile"),
-        actions: const [
+        actions: [
+          IconButton(
+            padding: const EdgeInsets.all(10),
+            icon: const Icon(Icons.save),
+            onPressed: () {
+              controller.save();
+            },
+          ),
           //! 4. Tambahkan tombol Save
           //! 5. Beri padding/margin pada tombol Save sebanyak 10
           //! 6. Panggil controller.save() ketika tombol di klik
@@ -20,12 +27,35 @@ class LtfmEditProfileView extends StatefulWidget {
         child: Container(
           padding: const EdgeInsets.all(10.0),
           child: Column(
-            children: const [
+            children: [
               //! 1. Buat sebuah Card, tambahkan Column di dalamnya
               //! 2. Tambahkan padding.all 20.0
               //! 3. Di dalam column yang ada di dalam Card, tambahkan field ini:
               //? textfield email
               //? textfield password
+              Card(
+                elevation: 2.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      QTextField(
+                        value: "demo@gmail.com",
+                        label: "Email",
+                        hint: "Your email",
+                        onChanged: (value) {},
+                      ),
+                      QTextField(
+                        value: "123456",
+                        hint: "Your password",
+                        label: "Password",
+                        obscure: true,
+                        onChanged: (value) {},
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
